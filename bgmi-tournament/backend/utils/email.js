7d257b41f481e2fe8202796d6c73a618c18cd5b4
tony0807133 <spalande092@gmail.com>
2026-03-18 01:43:09 +0530

    fix: set FROM to morse-code-trainer.com for Resend

--- bgmi-tournament/backend/utils/email.js
+++ bgmi-tournament/backend/utils/email.js
@@ -1,7 +1,7 @@
 const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM = 'BGMI Arena <onboarding@resend.dev>';
+const FROM = 'BGMI Arena <noreply@morse-code-trainer.com>';
 
 exports.sendRoomDetails = async ({ to, name, tournament, roomId, roomPassword, slotNumber }) => {
   await resend.emails.send({
